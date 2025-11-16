5cd300c6f4dc7259d58057c95f16a7aa2d2f7163
Patrick Claussen <claussenpatrick@gmail.com>
2025-11-16 18:06:49 -0300

    feat: Adicionando demais rotas ao CRUD para entidade Person na API
      - `GET /person`: Lista todas as pessoas.
      - `PUT /person/{id:guid}`: Atualiza uma pessoa existente.
      - `DELETE /person/{id:guid}`: Remove uma pessoa existente.

--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,5 +1,6 @@
 ﻿using EstudosApiRest.Data;
 using EstudosApiRest.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EstudosApiRest.Routes
 {
@@ -9,12 +10,45 @@ namespace EstudosApiRest.Routes
         {
             var route = app.MapGroup("/person");
 
+            route.MapGet("/", 
+                async (AppDbContext context) =>
+            {
+                return await context.People.ToListAsync();
+            });
+
             route.MapPost("/", 
                 async (PersonRequest req, AppDbContext context) =>
             {
                 var person = new PersonModel (req.name);
                 await context.AddAsync(person);
                 await context.SaveChangesAsync();
+            });
+
+            route.MapPut("/{id:guid}", 
+                async (Guid id, PersonRequest req, AppDbContext context) =>
+            {
+                var person = await context.People.FindAsync(id);
+                if (person is null)
+                {
+                    return Results.NotFound();
+                }
+                person.Name = req.name;
+                context.Update(person);
+                await context.SaveChangesAsync();
+                return Results.Ok();
+            });
+
+            route.MapDelete("/{id:guid}", 
+                async (Guid id, AppDbContext context) =>
+            {
+                var person = await context.People.FindAsync(id);
+                if (person is null)
+                {
+                    return Results.NotFound();
+                }
+                context.Remove(person);
+                await context.SaveChangesAsync();
+                return Results.Ok();
             });
         }
     }
